96236b12242e81c80242ee3735fb1c17bcc8227f
jbadelg <jbadel@uoc.edu>
2022-12-04 22:50:49 -0500

    initial commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,15 +2,13 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
+ //import * as bootstrap from 'bootstrap';
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+// console.log(bootstrap.Alert.VERSION);
